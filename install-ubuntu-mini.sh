022ac635d76b19b6d288e82931b68f80546a4af6
pbillerot <philippe.billerot@gmail.com>
2019-10-21 21:37:13 +0200

    baptème du feu

--- install-ubuntu-mini.sh
+++ install-ubuntu-mini.sh
@@ -4,25 +4,28 @@
 snap remove gnome-system-monitor
 snap remove gnome-calculator
 # Suppression de Firefox (sera remplacé par Chrome)
-snap remove firefox*
+sudo apt remove firefox* -y
 
 # Installation paquets standards
 
 # Google Chrome
-# Je n'utilise pas Chromium car non compatible avec Bureau à distance
+# Je n'utilise pas Chromium car ne permet pas le bureau à distance
 sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get install -y google-chrome-stable
 
 # Gnome + Système
-sudo apt install gnome-tweak-tool
-sudo apt install gnome-system-calculator
+sudo apt install -y gnome-tweak-tool
+sudo apt install -y gnome-system-calculator
 
 # Calculatrice
-sudo apt install gnome-calculator
+sudo apt install -y gnome-calculator
 # Lecteur multimédia
-sudo apt install vlc
+sudo apt install -y vlc
+
+# Nettoyage
+sudo apt autoremove -y
 
 # LibreOffice non nécessaire car documents gérés dans Google Drive
 # sudo apt-get install libreoffice libreoffice-gtk libreoffice-l10n-fr libreoffice-help-fr hyphen-fr
