bb16ed13c0c252f3c5f6cbbb3b57d4bd12ba0134
pbillerot <philippe.billerot@gmail.com>
2020-01-25 12:18:09 +0100

    Remplacement de Chrome par Chromium

--- install-ubuntu-mini.sh
+++ install-ubuntu-mini.sh
@@ -10,12 +10,8 @@ sudo apt autoremove --purge firefox* -y
 
 # Installation paquets standards
 
-# Google Chrome
-# Je n'utilise pas Chromium car ne permet pas le bureau à distance
-sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+# Chromium
+sudo apt install -y chromium-browser
 
 # Gnome + Système
 sudo apt install -y gnome-tweak-tool
